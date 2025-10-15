2c3273b2c3c1e6d01cf2791551d3c8def96823ca
Moiseev Ilya <ilya@mois.pro>
2025-10-15 19:40:44 +0400

    chore: update Vite configuration for dynamic base path and change output directory to dist

--- vite.config.ts
+++ vite.config.ts
@@ -2,32 +2,36 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
+const branch = process.env.BRANCH_NAME || 'main';
+const basePath =
+  branch === 'main'
+    ? '/demo-dapp-with-wallet/'
+    : `/demo-dapp-with-wallet/${branch}/`;
+
 export default defineConfig({
   plugins: [
     react(),
     nodePolyfills({
-      globals: {
-        Buffer: true,
-        // process: false 
-      },
+      globals: { Buffer: true },
       protocolImports: true,
     }),
   ],
   build: {
-    outDir: 'docs'
+    // Для Actions удобнее стандартный dist
+    outDir: 'dist',
   },
   resolve: {
-      alias: {
-          crypto: 'crypto-browserify',
-          'vite-plugin-node-polyfills/shims/process': 'process',
-          'node:process': 'process',
-          'vite-plugin-node-polyfills/shims/buffer': 'buffer',
-          'node:buffer': 'buffer',
-      },
+    alias: {
+      crypto: 'crypto-browserify',
+      'vite-plugin-node-polyfills/shims/process': 'process',
+      'node:process': 'process',
+      'vite-plugin-node-polyfills/shims/buffer': 'buffer',
+      'node:buffer': 'buffer',
+    },
   },
   optimizeDeps: { include: ['process', 'buffer'] },
-  // @ts-ignore
-  base: process.env.GH_PAGES ? '/demo-dapp-with-wallet/' : './',
+  // В проде (GH Pages) используем динамический base
+  base: process.env.GH_PAGES ? basePath : './',
   server: {
     fs: {
       allow: ['../sdk', './'],
