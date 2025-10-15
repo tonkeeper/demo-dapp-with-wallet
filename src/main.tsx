713d354981f207cea43f151522fe5f708837cbc9
Moiseev Ilya <ilya@mois.pro>
2025-10-15 20:05:05 +0400

    refactor: update main entry point to use createRoot for React 18 compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import './patch-local-storage-for-github-pages';
 
-import React, {StrictMode} from 'react'
-import { render } from 'react-dom';
-import App from './App'
-import './index.scss'
-import eruda from "eruda";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.scss';
+import eruda from 'eruda';
 
 eruda.init();
 
-render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-    document.getElementById('root') as HTMLElement
-)
+const container = document.getElementById('root');
+if (!container) throw new Error('Root element not found');
+
+createRoot(container).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
